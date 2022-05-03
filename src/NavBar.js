649cedd21cf66d138f8027ac01e34136331a02aa
Wen Chen <wen@Wens-MacBook-Air.local>
2022-05-03 15:26:03 -0700

    13min

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+        <nav>
+            <div className='logo'>Welcome</div>
+            <ul className='nav-links'>
+                <li><a>Home</a></li>
+                <li><a>Service</a></li>
+                <li><a>Product</a></li>
+                <li><a>Link</a></li>
+                </ul>
+            </nav>
+            
+        </div>
+  )
+}
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import Smarthomes from './Smart-homes.png'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <img src={Smarthomes} alt='Smart Home' />
     <App /> 
   </React.StrictMode>
 );
